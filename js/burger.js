a35a30f625dd9f907d61de1470662d4fe023dc38
Vadim <vluckjantsev501@gmail.com>
2023-04-01 14:22:50 +0300

    01042023-1

---
+++ js/burger.js
@@ -0,0 +1,6 @@
+let burgerContainer = document.querySelector(".burgerContainer")
+let burgerForm = document.querySelector(".burgerContainer__form")
+
+function showBurger(){
+    burgerContainer.classList.toggle('burger-active')
+}
